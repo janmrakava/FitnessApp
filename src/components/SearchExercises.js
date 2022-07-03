4046f8a03b80d4663870430c0978a77c1931076b
Jan Mrákava <66779580+janmrakava@users.noreply.github.com>
2022-07-03 21:31:07 +0200

    ExerciseCard with gif

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -26,10 +26,10 @@ const SearchExercises = ( {setExercises, bodyPart, setBodyPart} ) => {
       const exerciseData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions)
 
       const searchedExercises = exerciseData.filter(
-        (exercise) => exercise.name.toLowerCase().includes(search) 
-        || exercise.target.toLowerCase().includes(search) 
-        || exercise.equipment.toLowerCase().includes(search) 
-        || exercise.bodypart.toLowerCase().includes(search) 
+        (item) => item.name.toLowerCase().includes(search) 
+        || item.target.toLowerCase().includes(search) 
+        || item.equipment.toLowerCase().includes(search) 
+        || item.bodyPart.toLowerCase().includes(search) 
       );
 
       setSearch('')
